65a4cf14ae14d89cd051c0d74ef6fb8cf175e419
19029863 <19029863@LAPTOP-9KN5BD65>
2020-07-21 14:51:52 +0800

    modify doLoanChromebookTest

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -138,10 +138,14 @@ public class ResourceCentreTest {
 		// write your code here
 		
 		//Test if the list is populated so that an item can be loaned
+		assertNotNull("Test if list is available", chromebookList);
 		
 		//Test if an item in the list can be loaned
-				
+		assertEquals("Test if the first item isn't available", false, chromebookList.get(0).getIsAvailable());
+		chromebookList.get(0).setIsAvailable(false);
+		
 		//Test if the ArrayList is updated to reflect the loan
+		assertEquals("Test if in the ArrayList the first item has been loaned", true, chromebookList.get(0).getIsAvailable());
 				
 	}
 	
